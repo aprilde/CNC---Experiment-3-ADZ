23702160e2b21880583f2f4084065be8704cd9a5
April De Zen <aprildezen@aprilsmacbook3.acs.ocad.ca>
2018-11-02 15:38:35 -0400

    setup

---
+++ Arduino/arduino_Exp3_4buttons_ADZ/arduino_Exp3_4buttons_ADZ.ino
@@ -0,0 +1,57 @@
+/*
+ * Creation & Computation - Digital Futures, OCAD University
+ * Kate Hartman / Nick Puckett
+ * 
+ * April's Messy Hack 
+ * 
+ * 
+ */
+#include <ArduinoJson.h>
+//*****BE SURE TO INSTALL VERSION 5.13.3 OF THE LIBRARY IT INSTALL V6BETA BY DEFAULT 
+
+
+int button1 = 2;
+int button2 = 3;
+int button3 = 4;
+int button4 = 5;
+
+
+unsigned long lastSend;
+int sendRate = 50;
+void setup() 
+{
+  Serial.begin(9600);                    //turn on the serial port
+  pinMode(button1,INPUT_PULLUP);         //  
+  pinMode(button2,INPUT_PULLUP); 
+  pinMode(button3,INPUT_PULLUP); 
+  pinMode(button4,INPUT_PULLUP); 
+}
+
+void loop() 
+
+{
+  int buttonValue1 = digitalRead(button1);               //read button1
+  int buttonValue2 = digitalRead(button2);               //read button2
+  int buttonValue3 = digitalRead(button3);               //read button3
+  int buttonValue4 = digitalRead(button4);               //read button4
+  //int anaValue = analogRead(analogInPin);                 //read the analog value
+ 
+if(millis()-lastSend>=sendRate)                           //use a timer to stablize the data send
+{
+////////////////////////////////////////////////////////////send the values to P5 over serial
+  DynamicJsonBuffer messageBuffer(200);                   //create the Buffer for the JSON object        
+  JsonObject& p5Send = messageBuffer.createObject();      //create a JsonObject variable in that buffer       
+  
+  p5Send["b1"]=buttonValue1;                               //assign buttonValue to the key "s1" in the json object
+  p5Send["b2"]=buttonValue2;
+  p5Send["b3"]=buttonValue3;
+  p5Send["b4"]=buttonValue4;
+  //p5Send["s2"]=anaValue;                                  //assign anaValue to the key "s2" in the json object 
+
+  p5Send.printTo(Serial);                                 //print JSON object as a string
+  Serial.println();                                       //print a \n character to the serial port to distinguish between objects
+
+lastSend = millis();
+}  
+
+}
---
+++ P5/P5_Exp3_4buttons_ADZ/sketch.js
@@ -0,0 +1,74 @@
+//Creation&Computation
+//reads 2 sensor value from arduino
+//draws a circle based on the analog sensor value
+//change the color based on the button
+//uses JSON as the protocol
+//requires p5.serialcontrol to be running
+//and arduino running the SerialInput_1button_1ana sketch
+
+var serial;       //variable to hold the serial port object
+
+
+var button1;      //this variable will hold the value from "b1"
+var button2;
+var button3;
+var button4;
+
+//var memory= 0;
+
+var serialPortName = "/dev/cu.usbmodem272";  //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
+                              //Look at P5 Serial to see the available ports
+function setup() {
+  
+  createCanvas(2000,1000);
+  //Setting up the serial port
+  serial = new p5.SerialPort();     //create the serial port object
+  serial.open(serialPortName); //open the serialport. determined 
+  serial.on('open',ardCon);         //open the socket connection and execute the ardCon callback
+  serial.on('data',dataReceived);   //when data is received execute the dataReceived function
+}
+
+function draw() {
+  //background(255);
+  stroke(0);
+
+  
+  if(button1==1)
+      //if(button1==1 && memory == 0)
+  {
+    //memory = 1;
+  ellipse(random(0,2000), random(0,1000), 55, 55);
+  fill(255,0,0);
+  }
+/*    else
+    {
+      fill(0,255,0);
+    }
+*/
+  //strokeWeight(map(sensor2,0,1023,0.01,5));
+  
+  
+}
+
+
+function dataReceived()   //this function is called every time data is received
+{
+  
+var rawData = serial.readStringUntil('\r\n'); //read the incoming string until it sees a newline
+    console.log(rawData);                   //uncomment this line to see the incoming string in the console     
+    if(rawData.length>1)                      //check that there is something in the string
+    {                                         
+      
+      button1 = JSON.parse(rawData).b1;       //the parameter value .s1 must match the parameter name created within the arduino file
+      button2 = JSON.parse(rawData).b2; 
+      button3 = JSON.parse(rawData).b3;
+      button4 = JSON.parse(rawData).b4;
+    }
+}
+
+function ardCon()
+{
+  console.log("connected to the arduino!! Listen UP");
+}
+
+
