070ac21bea7c0626eb24134d2cecd4c31b616283
April De Zen <aprildezen@Aprils-MacBook-3.local>
2018-11-07 14:52:02 -0500

    getting the sensor to work

--- P5/P5_Exp3_4buttons_ADZ/sketch2.js
+++ P5/P5_Exp3_4buttons_ADZ/sketch2.js
@@ -7,7 +7,6 @@
 //and arduino running the SerialInput_1button_1ana sketch
 
 var serial;       //variable to hold the serial port object
-//var grumpImg;
 
 var button1;      //this variable will hold the value from "b1"
 var button2;
@@ -15,9 +14,12 @@ var button3;
 var button4;
 var lightSen = 0;
 
-//var memory= 0;
+var imgBtn1;
+var imgBtn2;
+var imgBtn3;
+var imgBtn4;
 
-var serialPortName = "/dev/cu.usbmodem279";  //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
+var serialPortName = "/dev/cu.usbmodem*";  //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
                               //Look at P5 Serial to see the available ports
 function preload(){
   grumpImg = loadImage("grump.png");
@@ -38,54 +40,45 @@ function setup() {
 
 function draw() {
   //background(0);
+console.log(lightSen);
   stroke(0);
     
-if(lightSen==1){
+if(lightSen <= 300)
+{
+    console.log("BRIGHT!!!!")
+    imgBtn1 = grumpImg;
+    imgBtn2 = grumpImg;
+    imgBtn3 = grumpImg;
+    imgBtn4 = grumpImg;
+}
+    else{
+        console.log("DARKKK!!!!")
+        imgBtn1 = CageImg;
+        imgBtn2 = CageImg;
+        imgBtn3 = CageImg;
+        imgBtn4 = CageImg;
+    }
   
   if(button1==1)
   {
-  image (grumpImg,random(0,2000), random(0,1000), grumpImg.width/10,grumpImg.height/10);
+  image (imgBtn1,random(0,2000), random(0,1000),width,height);
   }
 
-      if(button2==0)
+if(button2==0)
   {
 //  ellipse(random(0,2000), random(0,1000), 55, 55);
 //  fill(255,0,0);
-    image (SuccessImg,random(0,2000), random(0,1000), SuccessImg.width/10,SuccessImg.height/10);  
+    image (imgBtn2,random(0,2000), random(0,1000) ,width,height);  
   }
         if(button3==0)
   {
 //  ellipse(random(0,2000), random(0,1000), 55, 55);
 //  fill(255,0,0);
     //tint(255, 127);
-    image (CageImg,random(0,2000), random(0,1000), CageImg.width/5,CageImg.height/5);  
+    image (imgBtn3,random(0,2000), random(0,1000),width,height);  
     
-  }
-  }
-    else(lightSen==0){
-          if(button1==1)
-  {
-      ellipse(random(0,2000), random(0,1000), 55, 55);
-      fill(255,0,0);
-  //image (grumpImg,random(0,2000), random(0,1000), grumpImg.width/10,grumpImg.height/10);
   }
 
-      if(button2==0)
-  {
-ellipse(random(0,2000), random(0,1000), 55, 55);
-fill(255,0,0);
-    //image (SuccessImg,random(0,2000), random(0,1000), SuccessImg.width/10,SuccessImg.height/10);  
-  }
-        if(button3==0)
-  {
-ellipse(random(0,2000), random(0,1000), 55, 55);
-fill(255,0,0);
-    //tint(255, 127);
-    //image (CageImg,random(0,2000), random(0,1000), CageImg.width/5,CageImg.height/5);  
-    
-  }
-    }
-  
 }
 
 
@@ -94,13 +87,14 @@ function dataReceived()   //this function is called every time data is received
   
 var rawData = serial.readStringUntil('\r\n'); //read the incoming string until it sees a newline
     console.log(rawData);                   //uncomment this line to see the incoming string in the console     
-    if(rawData.length>1)                      //check that there is something in the string
+    if(rawData.length>=5)                      //check that there is something in the string
     {                                         
       
       button1 = JSON.parse(rawData).b1;       //the parameter value .s1 must match the parameter name created within the arduino file
       button2 = JSON.parse(rawData).b2; 
       button3 = JSON.parse(rawData).b3;
       button4 = JSON.parse(rawData).b4;
+    lightSen = JSON.parse(rawData).s1;
     }
 }
 
